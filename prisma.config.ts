e3d819607a2ff3ca3c6eee200f0c6c6238b9ed5c
Kritsada <ninep9p2006@gmail.com>
2026-09-03 19:14:15 +0700

    refactor: update prisma config to import env from prisma package and use function call for database url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
-import { env } from "./lib/env";
+import { defineConfig, env } from "prisma/config";
+
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +9,6 @@ export default defineConfig({
     seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
-    url: env.DATABASE_URL,
+    url: env("DATABASE_URL"),
   },
 });
